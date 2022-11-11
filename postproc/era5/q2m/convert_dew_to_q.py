a869b518f5a23f20705f36947fe8b9c992de83aa
Osamu Miyawaki <miyawaki@tungsten.cgd.ucar.edu>
2022-11-11 11:28:00 -0700

    initial

---
+++ postproc/era5/q2m/convert_dew_to_q.py
@@ -0,0 +1,30 @@
+import importlib
+import xarray as xr
+import numpy as np
+from metpy.units import units
+from metpy.calc import specific_humidity_from_dewpoint as td2q
+import sys
+from tqdm import tqdm
+
+idirp="/project/mojave/observations/ERA5_daily/PS/"
+idirtd="/project/mojave/observations/ERA5_daily/TD2m/"
+idirq="/project/mojave/observations/ERA5_daily/Q2m/"
+
+ystart=1960
+yend=2020
+
+for iyear in tqdm(np.arange(ystart,yend+1,1)):
+    fnamep = idirp+"ps_"+str(iyear)+".nc"
+    fnametd = idirtd+"td2m_"+str(iyear)+".nc"
+
+    dps = xr.open_dataset(fnamep)
+    dtd = xr.open_dataset(fnametd)
+    ps=dps['ps']*units.pascal
+    td=dtd['td2m']*units.kelvin
+    # calculate specific humidity
+    q2m = td2q(ps,td)
+
+    q2m = q2m.rename('q2m')
+    q2m.attrs={'units':'kg kg**-1'}
+    q2m.to_netcdf("/project/mojave/observations/ERA5_daily/Q2m/"+
+               "q2m_"+str(iyear)+".nc")
